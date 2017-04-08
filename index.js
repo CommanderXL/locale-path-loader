ece186a5beecaf32d5f2af79489388b4c29819d4
XRene <1017653702@qq.com>
2017-04-08 13:14:47 +0800

    添加map表生成

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// 配上query参数
 const loaderUtils = require('loader-utils')
+const lang = require('./lib/lang')
 module.exports = function (source) {
   this.cacheable && this.cacheable()
   let I18N_PATTERN = /\$\{locale\}/g
   let query = loaderUtils.getOptions(this) || {}
   // 多语言环境配置
   source = source.replace(I18N_PATTERN, query.locale || 'zh')
+
+  // 生成语言map表
+  if (query.inline && query.outputDir) {
+    lang(source, query.outputDir)
+  }
+
   return source
 }
